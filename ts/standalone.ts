efccbd5ad3c2a1ea98b1a0a33028b381893fb3c9
Vincent den Boer <vincent@shishkabab.net>
2020-05-19 17:15:01 +0200

    Open the management interface if trying to open the standalone version while its already running

--- ts/standalone.ts
+++ ts/standalone.ts
@@ -1,12 +1,20 @@
 import path from 'path'
-import { main } from "./main";
+const { open } = require('openurl')
+const tcpPortUsed = require('tcp-port-used')
 import { mkdirSync } from 'fs-extra';
+import { main, getPortNumber } from "./main";
 
 const AutoLaunch = require('auto-launch');
 
 export async function standalone() {
     if (process.env.NO_PRODUCTION !== 'true') {
         process.env.NODE_ENV = 'production'
+    }
+
+    const portNumber = getPortNumber()
+    if (await tcpPortUsed.check(portNumber)) {
+        await open(`http://localhost:${portNumber}/management`)
+        process.exit(0)
     }
 
     if (process.env.NO_AUTO_LAUNCH !== 'true') {
@@ -18,13 +26,17 @@ export async function standalone() {
     }
 
     const standaloneDirPath = path.dirname(process.argv[0])
+    const frontendDir = process.env.DB_PATH || path.join(standaloneDirPath, 'frontend')
     const dbFilePath = process.env.DB_PATH || path.join(standaloneDirPath, 'database')
     const pluginsDir = process.env.PLUGINS_DIR || path.join(standaloneDirPath, 'plugins')
     try {
         mkdirSync(pluginsDir)
     } catch (e) { }
 
-    await main({ runtimeConfig: { dbPath: dbFilePath, pluginsDir } })
+    await main({
+        runtimeConfig: { dbPath: dbFilePath, pluginsDir },
+        frontendDir
+    })
 }
 
 if (require.main === module) {
